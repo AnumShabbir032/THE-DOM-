ed1492f283c868337e14cf3aaa8b068715ae653f
AnumShabbir032 <AnumShabbir032@gmail.com>
2022-05-29 19:24:55 +0500

    THE DOM

---
+++ app.js
@@ -0,0 +1,41 @@
+// =========════============================ ⋆★⋆ ════===========================================
+// Chapter :58 to 67 "THE DOM"
+// =========════============================ ⋆★⋆ ════===========================================
+
+// ●○●○●○●○●●○●○●○●○● Question no :01 ●○●○●○●○●●○●○●○●○●
+
+// i. Get element of id “main-content” and assign them in a variable.
+
+// var main = document.getElementById("main-content");
+
+// ii. Display all child elements of “main-content” element.
+
+
+// var childElement = main.getElementsByTagName("p")
+
+// for(var i = 0 ; i < childElement.length ; i++){
+//     document.write(`${childElement[i]} <br />`);
+
+// }
+
+
+// iii. Get all elements of class “render” and show their innerHTML  in browser.
+
+// var classElements = main.getElementsByClassName("render");
+
+// for(var i = 0; i < classElements.length ; i++){
+//     document.write(`${classElements[i].innerHTML}<br />`)
+// }
+
+// iv. Fill input value whose element id first-name using javascript.
+
+// var input = document.getElementById("first-name");
+// input.value = "John";
+
+// v. Repeat part iv for id ”last-name” and “email”.
+
+
+// var input = document.getElementById("last-name");
+// input.value = "Thoms";
+
+//○●○●○●○●●○●○●○●○● ▃▃▃▃▃▃▃▃▃▃Chapter 58 to 67 End ▃▃▃▃▃▃▃▃▃▃▃ ○●○●○●○●●○●○●○●○●//
